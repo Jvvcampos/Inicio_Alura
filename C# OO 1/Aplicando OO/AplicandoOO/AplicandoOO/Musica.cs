58ce2260fe35cad0c1069691d9350c445ed7c407
Jvvcampos <joaovitorcampos161@gmail.com>
2025-11-17 10:32:05 -0300

    :sparkles: feat: Arquivos iniciais do curso 2 da formação Aprenda programar em C#
    - Códigos iniciais da solução do curso C# Aplicando orientação a Objetos.
    - Criando classe e métodos.

---
+++ C# OO 1/Aplicando OO/AplicandoOO/AplicandoOO/Musica.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound
+{
+    class Musica
+    {
+        public string nome;
+        public string artista;
+        public double duracao;
+        private bool disponivel;
+
+        public void EscreveDisponivel(bool value)
+        {
+            disponivel = value;
+        }
+
+        public bool LerDisponivel()
+        {
+            return disponivel;
+        }
+
+        public void ExibirFichaTecnica()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Artista: {artista}");
+            Console.WriteLine($"Duração: {duracao}");
+            if (disponivel)
+            {
+                Console.WriteLine("Disponível no plano.");
+            }
+            else
+            {
+                Console.WriteLine("Adquira o plano plus+");
+            }
+        }
+
+        public void ExibirNome()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Artista: {artista}");
+        }
+    }
+}
---
+++ C# OO 1/Aplicando OO/AplicandoOO/AplicandoOO/Program.cs
@@ -0,0 +1,22 @@
+﻿using ScreenSound;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Musica musica1 = new Musica();
+        musica1.nome = "Roxane";
+        musica1.artista = "The Police";
+        musica1.duracao = 100;
+        musica1.disponivel = true;
+
+        Musica musica2 = new Musica();
+        musica2.nome = "Vertigo";
+        musica2.artista = "U2";
+        musica2.duracao = 150;
+        musica2.disponivel = false;
+
+        musica1.ExibirFichaTecnica();
+        musica2.ExibirFichaTecnica();   
+    }
+}
