86322453da11539863d94e3c0b7da794f7d666ff
Jvvcampos <joaovitorcampos161@gmail.com>
2025-11-17 09:35:02 -0300

    :sparkles: feat: Arquivos do curso 1 da formação Aprenda programar em C#
    - Todos os arquivos do projeto do curso C# - Criando sua primeira aplicação.
    - Conta com o código desenvolvido dentro das aulas e alguns desafios mais significativos que foram propostos.

---
+++ C# OO 1/PrimeiroProjeto/DesafioAula2/Program.cs
@@ -0,0 +1,35 @@
+﻿Random numeroSecreto = new Random();
+int numeroAleatorio = numeroSecreto.Next(1, 101);
+
+Console.WriteLine("Bem-vindo ao jogo do número secreto em c#!");
+
+do
+{
+    Console.WriteLine("\nDigite um número entre 1 e 100:");
+    string input = Console.ReadLine()!;
+
+    if (!int.TryParse(input, out int entrada))
+    {
+        Console.WriteLine("Não é um número valido");
+        continue;
+    }
+
+    if(entrada < 1 || entrada > 100)
+    {
+        Console.WriteLine("Número inválido! Tente novamente.");
+        continue;
+    }
+    if(entrada < numeroAleatorio)
+    {
+        Console.WriteLine("O número tentado é menor que o número secreto!");
+        continue;
+    }
+    if(entrada > numeroAleatorio)
+    {
+        Console.WriteLine("O número tentado é maior que o número secreto!");
+        continue;
+    }
+     Console.WriteLine("Parabéns, você acertou!!!!");
+     break;
+
+} while (true);
