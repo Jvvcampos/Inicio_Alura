135848c293ca26cda9b84ab01f8dfd625a6206b5
Jvvcampos <joaovitorcampos161@gmail.com>
2025-11-26 09:53:04 -0300

    :wrench: build: Arquivos iniciais do curso 3 da formação Aprenda a programar em C#
    - Arquivos iniciais dos cursos anteriores que serão usados como base nesse curso 3 de "C# - Dominando Orientação a Objetos".

---
+++ C# OO 1/Dominando OO/ScreenSound/Program.cs
@@ -0,0 +1,148 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 10, 8});
+
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1:
+            RegistrarBanda();
+            break;
+        case 2:
+            MostrarBandasRegistradas();
+            break;
+        case 3:
+            AvaliarBanda();
+            break;
+        case 4:
+            CalculaMediaBanda();
+            break;
+        case -1:
+            Console.WriteLine("Tchau tchau :)");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+void RegistrarBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Registro de bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Bandas registradas");
+
+    foreach (string banda in bandasRegistradas.Keys)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int quantidadeLetras = titulo.Length;
+    string asteristicos = string.Empty.PadLeft(quantidadeLetras, '*');
+    Console.WriteLine(asteristicos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteristicos + "\n");
+}
+
+void AvaliarBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar bandas");
+    Console.Write("Digite o nome da banda à ser avaliada: ");
+    string nomeBanda = Console.ReadLine()!;
+
+    if (bandasRegistradas.ContainsKey(nomeBanda))
+    {
+        Console.Write($"Digite a nota que a banda {nomeBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeBanda].Add(nota);
+        Console.WriteLine($"A nota {nota} foi registrada para a banda {nomeBanda}.");
+        Console.WriteLine("Digite alguma tecla para voltar");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($"O nome da {nomeBanda} não foi encontrado!");
+        Console.WriteLine("Digite alguma tecla para voltar ao menu!");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
+void CalculaMediaBanda()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Média da nota de uma banda");
+    Console.Write("Digite o nome da banda que deseja ver a média: ");
+    string nomeBanda = Console.ReadLine()!;
+
+    if(bandasRegistradas.ContainsKey(nomeBanda))
+    {
+        Console.WriteLine($"A média da banda {nomeBanda} foi de {bandasRegistradas[nomeBanda].Average():F2}.");
+    }
+    else
+    {
+        Console.WriteLine("\nEssa banda ainda não foi registrada!");
+        Console.WriteLine("Digite alguma tecla para voltar");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
+ExibirOpcoesDoMenu();
---
+++ C# OO 1/Dominando OO/ScreenSound/ProgramC2.cs
@@ -0,0 +1,46 @@
+﻿/*using ScreenSound;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Banda queen = new Banda("Queen");
+        Album albumQueen = new Album("A Night at the Opera");
+
+        Musica musica = new Musica(queen, "Love of my Life")
+        {
+            Duracao = 213,
+            Disponivel = true,
+            Genero = new Genero { Nome = "Rock"}
+        };
+        musica.ExibirFichaTecnica();
+
+        Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+        {
+            Duracao = 354,
+            Disponivel = false,
+        };
+        musica2.ExibirFichaTecnica();
+
+        albumQueen.AdicionarMusica(musica);
+        albumQueen.AdicionarMusica(musica2);
+        albumQueen.ExibirMusicasAlbum();
+
+        queen.AdicionarAlbum(albumQueen);
+        queen.ExibirDiscografia();
+
+        Podcast podcast = new Podcast("Youtube", "Cronicas 24");
+        Episodio ep1 = new Episodio(10, 1, "Inicio afg");
+        Episodio ep2 = new Episodio(15, 2, "Inicio rbt");
+        
+        ep1.AdicionarConvidados("Joao");
+        ep1.AdicionarConvidados("Pedro");
+        ep2.AdicionarConvidados("Pedro");
+        ep2.AdicionarConvidados("Lucas");
+        podcast.AdicionarEpisodio(ep1);
+        podcast.AdicionarEpisodio(ep2);
+        podcast.ExibirDetalhes();
+
+    }
+}   */
