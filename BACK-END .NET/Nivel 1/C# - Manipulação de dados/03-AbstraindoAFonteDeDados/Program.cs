4281c11ddaad8166ddc594ed134a7e2cf69babd5
Jvvcampos <joaovitorcampos161@gmail.com>
2026-07-13 17:04:07 -0300

    :sparkles: feat: C#: Manipulação de dados com coleções e LINQ | Aula 3
    -  Aula 3 "Fluxo de manipulação de coleções" eu aprendi:
    - Como utilizar FileStream e StreamReader para ler arquivos e utilizar a cláusula using para gerenciar recursos.
    - A criar métodos que retornam IEnumerable<T> utilizando yield return para processar dados sob demanda.
    - A converter tipos em C#, usando Convert.ToInt32.
    - A criar métodos de extensão e utilizar delegates em C# para aplicar filtros em coleções de forma modular.
    - A usar expressões lambda para simplificar métodos em C#.
    - A manipular coleções usando a biblioteca LINQ para realizar operações de filtragem e transformação.
    - A encadear operações em coleções usando métodos de extensão para criar pipelines de dados.
    - A construir filtros dinâmicos usando expressões lambda sem a necessidade de criar métodos específicos para cada condição.

---
+++ BACK-END .NET/Nivel 1/C# - Manipulação de dados/03-AbstraindoAFonteDeDados/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+    Seja um arquivo com músicas em formato CSV (Comma Separated Values). 
+
+    Implemente as funções abaixo:
+    //     [ X ] Leia-o como uma coleção de músicas
+    //     [ X ] Filtre a coleção por artista (por ex. Coldplay, Metallica, AC/DC)
+    //     [  ] Filtre a coleção por gênero (por ex. rock)
+    //     [ ] Filtre a coleção por duração (por ex. maiores que 5 minutos)
+    //     [ ] Ordene a coleção por artista
+    //     [ ] Ordene a coleção por artista e em seguida por músicas com duração crescente
+    //     [ ] Crie uma coleção de artistas e suas músicas
+    //     [ ] Informe a duração média das músicas da coleção
+    //     [ ] Informe a duração total das músicas da coleção
+    //     [ ] Informe qual artista tem mais músicas na coleção
+ 
+*/
+using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var musicas =
+    ObterMusicas(stream)
+    .Where(musica => musica.Artista == "Coldplay")
+    .Where(musica => musica.Titulo.StartsWith('C'))
+    .Where(FiltrarMaisLongas);
+
+ExibirMusicas(musicas);
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var contador = 1;
+    Console.WriteLine($"\nExibindo as músicas: ");
+    foreach(var musica in musicas)
+    {
+        Console.WriteLine($"\t - {musica.Titulo} ({musica.Artista} - {musica.Duracao} seg)");
+        contador++;
+        if (contador > 50) break;
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader reader)
+{
+    var linha = stream.ReadLine();
+    while(linha is not null)
+    {
+        var leitura = linha.Split(';');
+        var musica = new Musica
+        {
+            Titulo = leitura[0],
+            Artista = leitura[1],
+            Duracao = Convert.ToInt32(leitura[2])
+        };
+        foreach (var leia in leitura)
+        {
+            if (leia.Contains(','))
+            {
+                var generos = leia.Split(',');
+                musica.Genero = generos[0];
+                musica.Genero2 = generos[1].TrimStart();
+            }
+        }
+        yield return musica;
+        linha = stream.ReadLine();
+    }
+}
+
+bool FiltrarPorArtista(Musica musica) => musica.Artista == "Coldplay";
+bool FiltrarMaisLongas(Musica m) => m.Duracao >= 300;
+bool FiltrarPorTituloComecaComA(Musica musica) => musica.Titulo.StartsWith('A');
+
+static class MusicasExtensions
+{
+    public static IEnumerable<Musica> FiltrarPorGenero(this IEnumerable<Musica> musicas,string genero)
+    {
+        foreach (var musica in musicas)
+        {
+            if (musica.Genero == genero || musica.Genero2 == genero) yield return musica;
+        }
+    }
+
+    public static IEnumerable<T> FiltrarPor<T>(this IEnumerable<T> musicas, Func<T, bool> condicao)
+    {
+        foreach (var musica in musicas)
+        {
+            if (condicao(musica)) yield return musica;
+        }
+    }
+}
+
+class Musica()
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public string Genero {  get; set; }
+    public string Genero2 { get; set; }
+}
